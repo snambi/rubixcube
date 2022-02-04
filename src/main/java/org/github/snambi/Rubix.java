0782edefc68de3d86bd8ca5a753284c203a871be
Nambi Sankaran <snambi@gmail.com>
2022-02-03 16:45:06 -0800

    initial commit

---
+++ src/main/java/org/github/snambi/Rubix.java
@@ -0,0 +1,15 @@
+package org.github.snambi;
+
+/**
+ * Rubix Cube App
+ *
+ *
+ */
+public class Rubix {
+
+    public static void main(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
+
+}
+
